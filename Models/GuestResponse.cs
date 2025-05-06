260654b7754ecf34e480334f6d39202437cfbc05
ka72tin@gmail.com <ka72tin@gmail.com>
2025-05-06 09:40:33 +0300

    3.2.1 Preparing the project 3.2.2 Adding a data model

---
+++ Models/GuestResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab3_1_templ.Models
+{
+    public class GuestResponse
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public bool? WillAttend { get; set; }
+    }
+}
